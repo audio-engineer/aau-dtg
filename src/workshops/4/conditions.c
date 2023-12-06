db264e76ffaa4d79aee95999af067841e071b1dc
Martin Kedmenec <martin.kedmenec@outlook.com>
2023-12-06 09:23:27 +0100

    Update code for workshop 4

--- src/workshops/4/conditions.c
+++ src/workshops/4/conditions.c
@@ -2,54 +2,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum { kTwo = 2, kFive = 5, kNine = 9 };
+enum Constants { kTwo = 2, kFive = 5, kNine = 9 };
 
-/* Denne funktion skal returnere 1 hvis x er et primtal og 0 ellers */
-bool IsPrime(int number) {
+bool IsPrimeNumber(int number) {
   for (int i = 2; i < number / 2; i++) {
     if (number % i == 0) {
       return false;
     }
   }
+
   return true;
 }
 
-/* Denne funktion skal returnere 1 hvis gcd(x,2)=1 og 0 ellers */
-bool IsGcd1(int number);
+bool IsGreatestCommonDivisorOne(int number_one, int number_two) {
+  // NOLINTNEXTLINE(readability-identifier-length)
+  int r = 0;
 
-/* Denne funktion skal returnere 1 hvis 9^x-2 mod 5 = 2 og 0 ellers */
-bool Is2Mod5(int number) {
-  int exponent_calculation = (int)pow(kNine, number % kFive);
-  // NOLINTNEXTLINE(readability-implicit-bool-conversion)
-  return (exponent_calculation - kTwo) % kFive == kTwo;
+  while (number_two != 0) {
+    r = number_one % number_two;
+
+    number_one = number_two;
+    number_two = r;
+  }
+
+  return (bool)(number_one == 1);
 }
 
-int main(void) {
-  int number_to_test = 0;
-  int is_prime_number = 0;
-  int is_greatest_common_divisor = 0;
-  int is_2mod5 = 0;
-  printf("Hvilket heltal vil du tjekke?\n");
+bool Is2Mod5(int number) {
+  int exponent_calculation = (int)pow(kNine, number % kFive);
+
+  return (bool)((exponent_calculation - kTwo) % kFive == kTwo);
+}
+
+int main() {
+  int number = 0;
+
+  printf("Enter a number: ");
+
   // NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling)
-  scanf("%d", &number_to_test);
-  printf("x er %d\n", number_to_test);
+  scanf("%d", &number);
 
-  // NOLINTNEXTLINE(readability-implicit-bool-conversion)
-  is_prime_number = IsPrime(number_to_test);
-  // NOLINTNEXTLINE(readability-implicit-bool-conversion)
-  is_greatest_common_divisor = IsGcd1(number_to_test);
-  // NOLINTNEXTLINE(readability-implicit-bool-conversion)
-  is_2mod5 = Is2Mod5(number_to_test);
+  bool is_prime_number = IsPrimeNumber(number);
+  bool is_greatest_common_divisor = IsGreatestCommonDivisorOne(number, 2);
+  bool is_2mod5 = Is2Mod5(number);
 
-  if ((is_prime_number && !is_2mod5) ||
-      !(is_prime_number || !is_greatest_common_divisor || is_2mod5) ||
-      (!is_prime_number && !is_greatest_common_divisor && is_2mod5)) {
-    printf("Du fandt et x\n");
-    printf("p er %d, q er %d, and r er %d\n", is_prime_number,
-           is_greatest_common_divisor, is_2mod5);
-  } else {
-    printf("Proev igen\n");
+  if (((int)is_prime_number && !is_2mod5) ||
+      !((int)is_prime_number || !is_greatest_common_divisor || (int)is_2mod5) ||
+      (!is_prime_number && !is_greatest_common_divisor && (int)is_2mod5)) {
+    printf("You found an x\n");
+    printf("p: %d, q: %d, r: %d\n", (int)is_prime_number,
+           (int)is_greatest_common_divisor, (int)is_2mod5);
+
+    return EXIT_SUCCESS;
   }
+
+  printf("Try again.\n");
 
   return EXIT_SUCCESS;
 }
