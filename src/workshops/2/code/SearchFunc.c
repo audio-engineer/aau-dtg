51a9ade8f91b6f6c88123dbf6ce7d7376577db88
Martin Kedmenec <martin.kedmenec@outlook.com>
2025-09-06 19:38:04 +0200

    Add workshop 2 code

---
+++ src/workshops/2/code/SearchFunc.c
@@ -0,0 +1,108 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> /* Nødvendig for tidtagning */
+
+#define N 500
+
+void LinSearch(int array[], int n, int x, int print);
+void BinSearch(int array[], int n, int x, int print);
+
+int main(void) {
+  int x;
+  char filename[20];
+  FILE* myFile;
+  int array[N];
+  int k;
+  int runs = 1000000;
+  int i;
+
+  sprintf(filename, "../List%d.txt", N);
+  printf("Indtast tallet du søger efter\n");
+  scanf("%d", &x);
+
+  myFile = fopen(filename, "r");
+
+  /* indlæs fil ind i array */
+  for (k = 0; k < N; k++) {
+    fscanf(myFile, "%d", &array[k]);
+  }
+  fclose(myFile);
+
+  printf("Listestørrelsen er %d\n", N);
+
+  /* Start tiden */
+  struct timespec startTime, endTime;
+  clock_gettime(CLOCK_MONOTONIC, &startTime);
+
+  for (i = 0; i < runs; i++) {
+    BinSearch(array, N, x, 0);
+  }
+
+  /* Slut tiden og print den */
+  clock_gettime(CLOCK_MONOTONIC, &endTime);
+  double duration = (double)(endTime.tv_sec - startTime.tv_sec) +
+                    1e-9 * (endTime.tv_nsec - startTime.tv_nsec);
+  printf("BinSearch brugte: %.7fs\n", duration);
+
+  /* Start tiden */
+  clock_gettime(CLOCK_MONOTONIC, &startTime);
+
+  for (i = 0; i < runs; i++) {
+    LinSearch(array, N, x, 0);
+  }
+
+  /* Slut tiden og print den */
+  clock_gettime(CLOCK_MONOTONIC, &endTime);
+  duration = (double)(endTime.tv_sec - startTime.tv_sec) +
+             1e-9 * (endTime.tv_nsec - startTime.tv_nsec);
+  printf("LinSearch brugte: %.7fs\n", duration);
+
+  /* Køres en gang for at printe resultat */
+  LinSearch(array, N, x, 1);
+  BinSearch(array, N, x, 1);
+  return 0;
+}
+
+void LinSearch(int array[], int n, int x, int print) {
+  int i;
+  /* Fyld ind hvad der mangler her */
+  while (i <= n && x != array[i]) {
+    i += 1;
+  }
+
+  if (i > n) {
+    i = 0;
+  }
+
+  if (print == 1) {
+    if (x == array[i]) {
+      printf("%d er på position %d\n", x, i + 1);
+    } else {
+      printf("%d er ikke i listen \n", x);
+    }
+  }
+}
+
+void BinSearch(int array[], int n, int x, int print) {
+  int i;
+  /* Fyld ind hvad der mangler her */
+  int m;
+
+  while (i < n && x != array[i]) {
+    m = (i + n) / 2;
+    if (x > array[m]) {
+      i = m + 1;
+    } else {
+      n = m;
+    }
+  }
+
+  if (print == 1) {
+    if (x == array[i]) {
+      printf("%d er på position %d\n", x, i + 1);
+    } else {
+      printf("%d er ikke i listen \n", x);
+    }
+  }
+}
